90030a0906f29ecf6383a9e8f93097d7c3f0a88e
nyl4gw <85944216+nyl4gw@users.noreply.github.com>
2021-08-10 11:14:45 -0400

    Update Data_importing_and_Cleaning.R

--- Data_importing_and_Cleaning.R
+++ Data_importing_and_Cleaning.R
@@ -9,14 +9,26 @@ white_wine <- dplyr::select(white_wine, -free.sulfur.dioxide)
 red_wine <- dplyr::select(red_wine, -free.sulfur.dioxide)
 
 red_wine <- red_wine %>%
-  mutate(color = 'red')
+  mutate(color = 0) #red is 0
 white_wine <- white_wine %>%
-  mutate(color = 'white')
+  mutate(color = 1) #white is 1
 
 wine_combined <- rbind(white_wine, red_wine) #combine datasets 
 
 wine_combined <- wine_combined %>%
-  mutate(quality_cat = ifelse(quality>=7, "high", "low"))
+  mutate(quality_cat = ifelse(quality>=7, 1, 0)) #high quality 1, low quality 0
+
+#change color to factor
+wine_combined$color<-factor(wine_combined$color)
+levels(wine_combined$color)
+levels(wine_combined$color) <- c("red","white") 
+levels(wine_combined$color)
+
+#change quality_cat to factor 
+wine_combined$quality_cat<-factor(wine_combined$quality_cat)
+levels(wine_combined$quality_cat)
+levels(wine_combined$quality_cat) <- c("low","high") 
+levels(wine_combined$quality_cat)
 
 #split into train and test data
 set.seed(1) #set seed 
@@ -31,9 +43,7 @@ train_red<-red_wine[sample_red, ] #training data
 test_red<-red_wine[-sample_red, ] 
 
 #combined
-sample_combined<-sample.int(nrow(wine_combined), floor(.80*nrow(wine_combined)), replace = F)
-train_combined<-wine_combined[sample_combined, ] #training data 
-test_combined<-wine_combined[-sample_combined, ] 
-
+train_combined <- rbind(train_white, train_red)
+test_combined <- rbind(test_white, test_red) 
 
 
